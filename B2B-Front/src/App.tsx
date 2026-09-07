ec74ae2f679a0de981e4ffb616f1262900b8ebd9
Dylan Rodríguez <dylanrodriguezchavarria@gmail.com>
2026-09-06 23:40:56 -0600

    add. Se cambia para que al bajar el tamaño de pantalla muestre un producto por columna

--- B2B-Front/src/App.tsx
+++ B2B-Front/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
                 ]}
               />
         <Hits hitComponent={Hit} classNames={{
-        list: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4",
+        list: "grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4",
       }} />
         
       </InstantSearch>
